307c1aa0cba90350e9311a4acebf4e139e729002
Lucas Oliveira <lucasantonio97@live.com>
2024-09-03 17:10:57 -0300

    Corrigindo erros de generics

--- src/main/java/generics/GenericsExempleMap.java
+++ src/main/java/generics/GenericsExempleMap.java
@@ -1,33 +1,39 @@
 package main.java.generics;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class GenericsExempleMap {
-	public static void main(String[] args) {
-		// Exemplo sem Generics
-		Map mapaSemGenerics = new HashMap();
-		mapaSemGenerics.put("Chave 1", 10);
-		mapaSemGenerics.put("Chave 2", "valor"); // Permite adicionar qualquer tipo de objeto
+    public static void main(String[] args) {
+        // Exemplo sem Generics
+        Map<String, Object> mapaSemGenerics = new HashMap<>();
+        mapaSemGenerics.put("Chave 1", 10);
+        mapaSemGenerics.put("Chave 2", "valor"); // Permite adicionar qualquer tipo de objeto
 
-		// Exemplo com Generics
-		Map<String, Integer> mapaGenerics = new HashMap<>();
-		mapaGenerics.put("Chave 1", 10);
-		mapaGenerics.put("Chave 2", 20);
+        // Exemplo com Generics
+        Map<String, Integer> mapaGenerics = new HashMap<>();
+        mapaGenerics.put("Chave 1", 10);
+        mapaGenerics.put("Chave 2", 20);
 
-		// Iterando sobre o mapa com Generics
-		for (Map.Entry<String, Integer> entry : mapaGenerics.entrySet()) {
-			String chave = entry.getKey();
-			int valor = entry.getValue();
-			System.out.println("Chave: " + chave + ", Valor: " + valor);
-		}
+        // Iterando sobre o mapa com Generics
+        for (Map.Entry<String, Integer> entry : mapaGenerics.entrySet()) {
+            String chave = entry.getKey();
+            int valor = entry.getValue();
+            System.out.println("Chave: " + chave + ", Valor: " + valor);
+        }
 
-		// Iterando sobre o mapa sem Generics (necessário fazer cast)
-		for (Object obj : mapaSemGenerics.entrySet()) {
-			Map.Entry entry = (Map.Entry) obj;
-			String chave = (String) entry.getKey();
-			Object valor = entry.getValue();
-			System.out.println("Chave: " + chave + ", Valor: " + valor);
-		}
-	}
+        // Iterando sobre o mapa sem Generics (verificar o tipo antes de fazer o cast)
+        for (Map.Entry<String, Object> entry : mapaSemGenerics.entrySet()) {
+            String chave = entry.getKey();
+            Object valor = entry.getValue();
+            if (valor instanceof Integer) {
+                int intValue = (Integer) valor;
+                System.out.println("Chave: " + chave + ", Valor: " + intValue);
+            } else if (valor instanceof String) {
+                String strValue = (String) valor;
+                System.out.println("Chave: " + chave + ", Valor: " + strValue);
+            } else {
+                System.out.println("Chave: " + chave + ", Valor: " + valor);
+            }
+        }
+    }
 }
--- src/main/java/generics/GenericsExempleSet.java
+++ src/main/java/generics/GenericsExempleSet.java
@@ -1,29 +1,32 @@
 package main.java.generics;
-
 import java.util.HashSet;
 import java.util.Set;
 
 public class GenericsExempleSet {
-	public static void main(String[] args) {
-		// Exemplo sem Generics
-		Set conjuntoSemGenerics = new HashSet();
-		conjuntoSemGenerics.add("Elemento 1");
-		conjuntoSemGenerics.add(10); // Permite adicionar qualquer tipo de objeto
+    public static void main(String[] args) {
+        // Exemplo sem Generics
+        Set<Object> conjuntoSemGenerics = new HashSet<>();
+        conjuntoSemGenerics.add("Elemento 1");
+        conjuntoSemGenerics.add(10); // Permite adicionar qualquer tipo de objeto
 
-		// Exemplo com Generics
-		Set<String> conjuntoGenerics = new HashSet<>();
-		conjuntoGenerics.add("Elemento 1");
-		conjuntoGenerics.add("Elemento 2");
+        // Exemplo com Generics
+        Set<String> conjuntoGenerics = new HashSet<>();
+        conjuntoGenerics.add("Elemento 1");
+        conjuntoGenerics.add("Elemento 2");
 
-		// Iterando sobre o conjunto com Generics
-		for (String elemento : conjuntoGenerics) {
-			System.out.println(elemento);
-		}
+        // Iterando sobre o conjunto com Generics
+        for (String elemento : conjuntoGenerics) {
+            System.out.println(elemento);
+        }
 
-		// Iterando sobre o conjunto sem Generics (necessário fazer cast)
-		for (Object elemento : conjuntoSemGenerics) {
-			String str = (String) elemento;
-			System.out.println(str);
-		}
-	}
+        // Iterando sobre o conjunto sem Generics (verificar o tipo antes de fazer o cast)
+        for (Object elemento : conjuntoSemGenerics) {
+            if (elemento instanceof String) {
+                String str = (String) elemento;
+                System.out.println(str);
+            } else {
+                System.out.println("Elemento não é uma String: " + elemento);
+            }
+        }
+    }
 }
